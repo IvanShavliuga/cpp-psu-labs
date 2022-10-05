dc953286f3ae194238f9ac00254d3e889ea22d9f
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2022-10-05 14:34:44 +0300

    correct code

--- z2v16/main.c
+++ z2v16/main.c
@@ -21,57 +21,57 @@
  *           00/00/00  Created                                              *
  *                                                                          *
  ****************************************************************************/
-//Типовые варианты аналогично
+//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 int main(int argc, char *argv[])
 {
   /* TODO: Enter code here */
-  char a,b,c; // Сотни десятки еденицы
+  char a,b,c; // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
   scanf("%c%c%c",&a,&b,&c);
   switch(a){
-  case '1' printf("Сто "); break;
-  case '2' printf("Двести "); break;
-  case '3' printf("Тристо "); break;
-  case '4' printf("Четыресто "); break;
-  case '5' printf("Пятьсот "); break;
-  case '6' printf("Шестьсот "); break;
-  case '7' printf("Семьсот "); break;
-  case '8' printf("Восемьсот "); break;
-  case '9' printf("Девятьсот "); break;
+  case '1': printf("пїЅпїЅпїЅ "); break;
+  case '2': printf("пїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '3': printf("пїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '4': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '5': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '6': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '7': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '8': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '9': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
   }
   switch(b){
-  case '1' 
+  case '1': 
     switch(c){
-    case '0': printf("десять\n "); return 0;
-    case '1' printf("одиннадцать\n "); return 0;
-    case '2' printf("двенадцать \n"); return 0;
-    case '3' printf("тринадцать\n "); return 0;
-    case '4' printf("четырнадцать\n "); return 0;
-    case '5' printf("пятнадцать \n"); return 0;
-    case '6' printf("шестнадцать\n"); return 0;
-    case '7' printf("семнадцать \n"); return 0;
-    case '8' printf("восемнадцать \n"); return 0;
-    case '9' printf("девятнадцать\n"); return 0;
+    case '0': printf("пїЅпїЅпїЅпїЅпїЅпїЅ\n "); return 0;
+    case '1': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n "); return 0;
+    case '2': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ \n"); return 0;
+    case '3': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n "); return 0;
+    case '4': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n "); return 0;
+    case '5': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ \n"); return 0;
+    case '6': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n"); return 0;
+    case '7': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ \n"); return 0;
+    case '8': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ \n"); return 0;
+    case '9': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ\n"); return 0;
     }
     break;
-  case '2' printf("двадцать "); break;
-  case '3' printf("тридцать "); break;
-  case '4' printf("сорок "); break;
-  case '5' printf("пятьдесят "); break;
-  case '6' printf("шестьдесят "); break;
-  case '7' printf("семьдесят "); break;
-  case '8' printf("восемьдесят "); break;
-  case '9' printf("девяносто "); break;
+  case '2': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '3': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '4': printf("пїЅпїЅпїЅпїЅпїЅ "); break;
+  case '5': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '6': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '7': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '8': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '9': printf("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ "); break;
   }
   switch(c){
-  case '1' printf("один "); break;
-  case '2' printf("два "); break;
-  case '3' printf("три "); break;
-  case '4' printf("четыре "); break;
-  case '5' printf("пять "); break;
-  case '6' printf("шесть"); break;
-  case '7' printf("семь "); break;
-  case '8' printf("восемь "); break;
-  case '9' printf("девять"); break;
+  case '1': printf("пїЅпїЅпїЅпїЅ "); break;
+  case '2': printf("пїЅпїЅпїЅ "); break;
+  case '3': printf("пїЅпїЅпїЅ "); break;
+  case '4': printf("пїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '5': printf("пїЅпїЅпїЅпїЅ "); break;
+  case '6': printf("пїЅпїЅпїЅпїЅпїЅ"); break;
+  case '7': printf("пїЅпїЅпїЅпїЅ "); break;
+  case '8': printf("пїЅпїЅпїЅпїЅпїЅпїЅ "); break;
+  case '9': printf("пїЅпїЅпїЅпїЅпїЅпїЅ"); break;
   }
   printf("\n");
   return 0;
